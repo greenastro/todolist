c21e59ae8770472dc9665ea26703441d400afd66
greenastro <greenastro@naver.com>
2020-10-15 10:59:32 +0900

    Updates

---
+++ precache-manifest.5ccdf1f8f62603b50796197659d856fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb3d7cc985e49df63c8d907e864d391",
+    "url": "/greenastro/todolist.git/index.html"
+  },
+  {
+    "revision": "ad706d45286a4afe2d7d",
+    "url": "/greenastro/todolist.git/static/css/main.9be5968a.chunk.css"
+  },
+  {
+    "revision": "eee7cdfb492da4bc2ae6",
+    "url": "/greenastro/todolist.git/static/js/2.d5091432.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/greenastro/todolist.git/static/js/2.d5091432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad706d45286a4afe2d7d",
+    "url": "/greenastro/todolist.git/static/js/main.3629eb05.chunk.js"
+  },
+  {
+    "revision": "e8a80b361f0b75de494b",
+    "url": "/greenastro/todolist.git/static/js/runtime-main.11cfe7b5.js"
+  }
+]);
